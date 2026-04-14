ef17b73f92e60125a52f736770e52356e25a066d
GuilhermeMaglia <guipmagliad@gmail.com>
2026-04-14 16:08:41 -0300

    Criação das Procedures -relVendas- e -visualizaVendas-

--- avaliacao/procedures.sql
+++ avaliacao/procedures.sql
@@ -42,3 +42,52 @@ DELIMITER //
     END //
 
 DELIMITER ;
+
+/*
+7) Crie uma stored procedure relVendas(idUsuario) que receba o id de um usuário e liste todas as
+ parcelas associadas, juntamente com a situação de pagamento de cada uma
+*/
+
+DELIMITER //
+
+    CREATE PROCEDURE relVendas(IN p_idUsuario INT)
+    BEGIN
+        SELECT
+            u.nome AS usuario,
+            f.tituloPortugues AS Filme,
+            v.id AS ID_Venda,
+            p.id AS ID_Parcela,
+            p.valor AS Valor_Parcela,
+            p.vencimento AS Vencimento,
+            p.situacao AS Status_Pagamento
+        FROM usuario u
+        JOIN venda v ON u.id = v.idUsuario 
+        JOIN sessao s ON v.idSessao = s.id 
+        JOIN filme f ON s.idFilme = f.id 
+        JOIN parcela p ON v.id = p.idVenda
+        WHERE u.id = p_idUsuario
+        ORDER BY p.vencimento ASC;
+END //
+
+DELIMITER ;
+
+/*
+8) Crie uma stored procedure visualizaVendas(tipoVenda) 
+que receba um parâmetro (1 para à vista, 2 para parcelado) e 
+retorne o somatório do valor das vendas para o tipo de pagamento selecionado.
+*/
+
+DELIMITER //
+
+    CREATE PROCEDURE visualizaVendas (IN p_idVenda INT)
+    BEGIN
+        SELECT 
+            tp.nome AS Tipo_Pagamento,
+            COALESCE(SUM(v.valorIngresso), 0) AS Total_Arrecadado
+        FROM tipoPagto tp
+        LEFT JOIN venda v ON tp.id = v.idTipoPagto
+        WHERE tp.id = p_idVenda
+        GROUP BY tp.nome;
+END //
+
+DELIMITER ;
