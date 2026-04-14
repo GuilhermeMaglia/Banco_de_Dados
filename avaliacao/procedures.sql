5b15bd0a37af3be87957884cdb7e8cbd9028bcfb
GuilhermeMaglia <guipmagliad@gmail.com>
2026-04-14 10:02:23 -0300

    Criação de Outros Arquivos para Organização

---
+++ avaliacao/procedures.sql
@@ -0,0 +1,5 @@
+/*
+5) Crie uma stored procedure chamada alteraValorIngresso(valor) 
+que altere o valor de todos os ingressos vendidos para o valor fornecido como parâmetro. 
+Após a atualização, a procedure deve listar os ingressos afetados. 
+*/
